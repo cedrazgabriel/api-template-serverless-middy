efd84c43f13994d5d4ef598ede06af5c35f2351e
Gabriel Cedraz <cedrazdev@gmail.com>
2024-12-26 13:32:53 -0300

    chore: implementa middy no projeto

--- src/hello.ts
+++ src/hello.ts
@@ -1,6 +1,10 @@
-export async function handler() {
-    return {
+import middy from '@middy/core'
+
+export const handler = middy(async () => {
+    return{
         statusCode: 200,
-        body: JSON.stringify({ message: 'Hello Cedraz' }),
+        body: JSON.stringify({
+            message: 'Hello Cedraz from middy'
+        })
     }
-}
+})
