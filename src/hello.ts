fd3a36c1ad4c1837bf608f01a315aded08da4cd6
Gabriel Cedraz <cedrazdev@gmail.com>
2024-12-26 15:09:03 -0300

    feat: implementa middleware de json parse do middy

--- src/hello.ts
+++ src/hello.ts
@@ -1,7 +1,8 @@
+import { APIGatewayProxyEventV2 } from 'aws-lambda'
 import { makeHandler } from './middy/make-handler'
 
-export const handler = makeHandler(async () => {
-    return{
+export const handler = makeHandler(async (event: APIGatewayProxyEventV2) => {
+    return {
         statusCode: 200,
         body: JSON.stringify({
             message: 'Hello Cedraz from middy'
--- src/middy/make-handler.ts
+++ src/middy/make-handler.ts
@@ -1,7 +1,11 @@
 import middy from "@middy/core";
+import httpJsonBodyParser from '@middy/http-json-body-parser'
+import { APIGatewayProxyEventV2 } from "aws-lambda";
 
-type Handler = (event: any) => Promise<any>
+type Handler = (event: APIGatewayProxyEventV2) => Promise<any>
 
 export function makeHandler(handler : Handler) {
-    return middy().handler(handler)
+    return middy()
+    .use(httpJsonBodyParser({disableContentTypeError: true}))
+    .handler(handler)
 }
