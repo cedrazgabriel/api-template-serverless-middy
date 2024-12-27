bfb1fc71d8ec074354e1301e8dc8f3868d697561
Gabriel Cedraz <cedrazdev@gmail.com>
2024-12-27 10:49:57 -0300

    implementa envio de arquivo ao s3

--- src/upload.ts
+++ src/upload.ts
@@ -1,7 +1,10 @@
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { randomUUID } from 'node:crypto';
 import { makeHandler } from './middy/make-handler'
 import { IFile } from './types/IFile';
+import { s3Cliente } from './clients/s3-client';
 
-interface  IUploadRequestBody {
+interface IUploadRequestBody {
     firstName: string
     lastName: string
     file: IFile;
@@ -9,10 +12,22 @@ interface  IUploadRequestBody {
 
 
 export const handler = makeHandler<IUploadRequestBody>(async (request) => {
+    const { file } = request.body
+
+    const newFileName = `${randomUUID()}-${file.filename}`
+
+    const putObjectCommand = new PutObjectCommand({
+        Bucket: "cedraz-bucket",
+        Key: newFileName,
+        Body: file.content,
+    })
+
+    await s3Cliente.send(putObjectCommand)
+
     return {
         statusCode: 200,
         body: {
-            filename: request.body.file.filename,
+            filename: newFileName,
         },
     }
 })
